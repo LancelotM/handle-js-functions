1c004cf83ca6806fad2f4718beea7e592296d497
LancelotM <yxm134679@163.com>
2021-06-20 20:21:25 +0800

    #Add arrUnique

---
+++ js/handleFunctions/arrUnique.js
@@ -0,0 +1,41 @@
+/**
+ * 数组去重
+ */
+
+const arr = [1,1,'1',17,true,true,false,false,'true','a',{},{}]
+
+const res1 = Array.from(new Set(arr));
+console.log('res1',res1)
+
+const res2 = [...new Set(arr)]
+console.log('res2',res2)
+
+const unique1 = arr => {
+    const res = []
+    for(let val of arr){
+        // if(res.indexOf(val) === -1) res.push(val)
+        if(!res.includes(val)) res.push(val)
+    }
+    return res;
+}
+console.log('res3',unique1(arr))
+
+const unique2 = arr => {
+    return arr.filter((item,index)=>{
+        return arr.indexOf(item) === index
+    })
+}
+console.log('res4',unique2(arr))
+
+const unique3 = arr => {
+    const map = new Map();
+    const res = []
+    for(let val of arr){
+        if(!map.has(val)){
+            map.set(val,true);
+            res.push(val);
+        }
+    }
+    return res;
+}
+console.log('res5',unique3(arr))
